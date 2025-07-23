91214c1ca1ebaf32ed2b9a86e42aa0fcd0dd4529
Xiangkun Hu <xkhu17@fudan.edu.cn>
2025-07-23 16:35:34 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://gair-nlp.github.io',
+  url: 'https://asi4ai.ai',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ASI4AI',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
